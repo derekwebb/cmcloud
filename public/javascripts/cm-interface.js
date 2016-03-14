51db98a98ca7e7acc81b28253a897a150acd7bc1
Derek Webb <derek.webb@coraltechgroup.com>
2016-03-14 10:17:37 -0400

    Added moment.js and updated user profile and lists.

--- public/javascripts/cm-interface.js
+++ public/javascripts/cm-interface.js
@@ -1,3 +1,23 @@
+var cmInterface = {
+	defaultFormat: "MM-DD-YYYY",
+	formatDate: function(timestamp, dateFormat) {
+		var dateFormat = (dateFormat) ? dateFormat : cmInterface.defaultFormat;
+		var time = moment(Number(timestamp));
+		var formatted = moment(time).format(dateFormat);
+		return formatted;
+	}
+};
+
+
 $( document ).ready(function() {
-  tinymce.init({ selector:'.profile-edit textarea' });
+	if (typeof tinymce !== 'undefined') {
+  	tinymce.init({ selector:'.profile-edit textarea' });
+	}
+
+	if ($('.member-since .date').length) {
+		$('.member-since .date').each(function(i) {
+			var ts = $(this).text();
+			$(this).text(cmInterface.formatDate(ts));
+		});
+	}
 });
